c28e42e194ba6362d25dec8a7155345486680102
Fooxer <fooxer9@users.noreply.github.com>
2019-05-16 14:34:10 +0300

    Полная рабочая версия
    1) Загружает сохраненные данные
    2) Добавляет запись
    3) Обновляет последнюю запись
    
    Очистка бд, если в ней что-то есть на момент запуска - в след коммите

--- app/src/main/java/msc/fooxer/studdatabase/MainActivity.kt
+++ app/src/main/java/msc/fooxer/studdatabase/MainActivity.kt
@@ -1,16 +1,20 @@
 package msc.fooxer.studdatabase
 
 import android.content.ContentValues
+import android.content.Intent
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Math.random
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +31,8 @@ class MainActivity : AppCompatActivity() {
         // Форматирование времени как "день.месяц.год"
         var dateFormat: DateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
 
+        val NOTES = ArrayList<Item>()
+
     }
     lateinit var db: SQLiteDatabase
     lateinit var dbHelper: DBHelper
@@ -38,27 +44,92 @@ class MainActivity : AppCompatActivity() {
         val contentValues = ContentValues()
         val random = Random()
         var dateText = dateFormat.format(Date())
+        val cursor: Cursor = db.query(TABLE_NAME,null,null,null,null,null,null)
+       /* if (cursor.moveToFirst()) {
+            db.delete(TABLE_NAME, null, null) // если есть записи - удалить все
+        }*/
+            for (i in 0..5) {
+                val FIO: String =
+                    "${secondName[random.nextInt(secondName.size)]} ${firstName[random.nextInt(firstName.size)]}" +
+                            " ${thirdName[random.nextInt(thirdName.size)]}"
+                contentValues.put(KEY_NAME, FIO)
+                contentValues.put(KEY_TIME, dateText)
+                db.insert(TABLE_NAME, null, contentValues)
+            }
 
-        for(i in 0..5){
-            val FIO: String = "${secondName[random.nextInt(secondName.size)]} ${firstName[random.nextInt(firstName.size)]}" +
-                    " ${thirdName[random.nextInt(thirdName.size)]}"
-            contentValues.put(KEY_NAME,FIO)
-            contentValues.put(KEY_TIME, dateText)
-            db.insert(TABLE_NAME,null,contentValues)
+        //db.delete(TABLE_NAME, "$KEY_INDEX > 5", null)
+        getColumns()
+        cursor.close()
+
+        openBDButton.setOnClickListener{
+            startActivity(Intent(this,DataBaseActivity::class.java))
+        }
+        newNoteButton.setOnClickListener{
+            addNew()
+            Snackbar.make(it, R.string.add, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+
+        }
+
+        lastNoteChangeButton.setOnClickListener {
+            update()
+            Snackbar.make(it, R.string.magic, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
-    fun getColumns(view: View) {
+    fun getColumns() {
         val cursor: Cursor = db.query(TABLE_NAME,null,null,null,null,null,null)
         if(cursor.moveToFirst()) {
             val idIndex = cursor.getColumnIndex(KEY_INDEX)
             val nameIndex = cursor.getColumnIndex(KEY_NAME)
             val dateIndex = cursor.getColumnIndex(KEY_TIME)
             do {
+                val item = Item (cursor.getInt(idIndex),cursor.getString(nameIndex),cursor.getString(dateIndex))
                 Log.d("mLog", "ID =  ${cursor.getInt(idIndex)} NAME = ${cursor.getString(nameIndex)}" +
                         " DATE = ${cursor.getString(dateIndex)}")
+                NOTES.add(item)
             } while (cursor.moveToNext())
         }
         cursor.close()
+    }
+
+    fun addNew() {
+        val cv = ContentValues()
+        val random = Random()
+        val dateText = dateFormat.format(Date())
+        val FIO: String =
+            "${secondName[random.nextInt(secondName.size)]} ${firstName[random.nextInt(firstName.size)]}" +
+                    " ${thirdName[random.nextInt(thirdName.size)]}"
+        cv.put(KEY_NAME, FIO)
+        cv.put(KEY_TIME, dateText)
+        db.insert(TABLE_NAME, null, cv)
+        val cursor = db.query(TABLE_NAME,null,null,null,null,null,null)
+        cursor.moveToLast()
+        val idIndex = cursor.getColumnIndex(KEY_INDEX)
+        val nameIndex = cursor.getColumnIndex(KEY_NAME)
+        val dateIndex = cursor.getColumnIndex(KEY_TIME)
+        NOTES.add(Item(cursor.getInt(idIndex), cursor.getString(nameIndex), cursor.getString(dateIndex)))
+        cursor.close()
+
+    }
+
+    fun update() {
+        val cursor = db.query(TABLE_NAME,null,null,null,null,null,null)
+        cursor.moveToLast()
+        val cv = ContentValues()
+        val idIndex = cursor.getColumnIndex(KEY_INDEX)
+        val nameIndex = cursor.getColumnIndex(KEY_NAME)
+        val dateIndex = cursor.getColumnIndex(KEY_TIME)
+        cv.put(KEY_INDEX,cursor.getInt(idIndex))
+        cv.put(KEY_NAME, "Иванов Иван Иванович")
+        cv.put(KEY_TIME, cursor.getString(dateIndex))
+        val updCount =  db.update(TABLE_NAME, cv,"$KEY_INDEX = ${cursor.getInt(idIndex)}", null)
+        Log.d("mLog", "updated rows count = " + updCount)
+        cursor.close()
+        val newCursor = db.query(TABLE_NAME,null,null,null,null,null,null)
+        newCursor.moveToLast()
+        NOTES.set(NOTES.lastIndex,Item(newCursor.getInt(idIndex),newCursor.getString(nameIndex),newCursor.getString(dateIndex)) )
+        newCursor.close()
     }
 }
--- app/src/main/java/msc/fooxer/studdatabase/RecyclerAdapter.kt
+++ app/src/main/java/msc/fooxer/studdatabase/RecyclerAdapter.kt
@@ -1,22 +1,31 @@
 package msc.fooxer.studdatabase
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class RecyclerAdapter internal constructor (val context: Context, val items: ArrayList <Item>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.element, viewGroup, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return items.size
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onBindViewHolder(view: ViewHolder, position: Int) {
+        val item = items[position]
+        view.idTextView.text = item.id.toString()
+        view.nameTextView.text = item.name
+        view.dateTextView.text = item.date
+
+        }
 
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val idTextView = view.findViewById(R.id.element_index) as TextView
